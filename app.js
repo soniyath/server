5e8c4d651b2a1da3f734b832620ae43c18edf6a2
soniyath <yatharth.soni14@gmail.com>
2020-11-23 09:52:05 -0500

    Updated with SocketIO

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
+// const express = require("express");
+// const app = express();
+
+// app.get("/welcome", function(req, res){
+//     res.send("Welcome to Yatharth Soni's Web Server");
+// });
+
+// const PORT = 8080;
+// app.listen(PORT, function(){
+//     console.log("Server is ready at "+ PORT);
+// });
+
+
+//Above code works perfectly the code below if for experimenting
+
 const express = require("express");
 const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io") (server);
+const port = 3000;
 
-app.get("/welcome", function(req, res){
-    res.send("Welcome to Yatharth Soni's Web Server");
-});
+io.on("connection", socket => {
+    console.log("a user connected :D");
+    socket.on("chat message", msg => {
+      console.log(msg);
+      io.emit("chat message", msg);
+    });
+  });
 
-const PORT = 8080;
-app.listen(PORT, function(){
-    console.log("Server is ready at "+ PORT);
-});
+  server.listen(port, () => console.log("server running on port:" + port));
